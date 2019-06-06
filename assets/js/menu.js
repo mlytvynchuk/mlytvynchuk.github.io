05adc9c94c4ff7a9b39813052b8da4cdc9b37197
Maksym <lytvynchuk.official@gmail.com>
2019-06-06 11:53:52 +0300

    fix body scrolling

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,21 +1,34 @@
 function notDisplayModal() {
     var menu = document.getElementById('filter-container');
+
     menu.className += 'menu-container notDisplay';
+    var container = document.getElementsByTagName('body')[0]
+    container.className = '';
 }
 
 function displayModal() {
     var menu = document.getElementById('filter-container');
     menu.className += 'menu-container';
+    var container = document.getElementsByTagName('body')[0]
+    container.className += 'modal-open';
+
+
 }
 
 function displayDiscount() {
     var menu = document.getElementById('discount-modal');
     menu.className += 'menu-container';
+    var container = document.getElementsByTagName('body')[0]
+    container.className += 'modal-open';
+
 
 }
 
 function notDisplayDiscount() {
     var menu = document.getElementById('discount-modal');
-    menu.className = 'menu-container';
+
     menu.className += ' notDisplay';
+    var container = document.getElementsByTagName('body')[0]
+    container.className = '';
+
 }
