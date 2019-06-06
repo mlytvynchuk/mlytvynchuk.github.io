8e9816d74aa8dcef0d6e549c3d8759d479d5b0e9
Admin <admin@eltec.edvantis.com>
2019-06-06 15:06:13 +0300

    page with profile

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -32,3 +32,28 @@ function notDisplayDiscount() {
     container.className = '';
 
 }
+/*for user profile*/ 
+function displayAddDiscount() {
+    var menu = document.getElementById('add-discount');
+    menu.className += 'menu-container';
+
+}
+
+function notDisplayAddDiscount() {
+    var menu = document.getElementById('add-discount');
+    menu.className = 'menu-container';
+    menu.className += ' notDisplay';
+}
+
+
+function displayLikes() {
+    var menu = document.getElementById('likes-container');
+    menu.className += 'menu-container';
+
+}
+
+function notDisplayLikes() {
+    var menu = document.getElementById('likes-container');
+    menu.className = 'menu-container';
+    menu.className += ' notDisplay';
+}
